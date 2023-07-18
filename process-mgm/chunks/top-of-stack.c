1ea3894efea3a1170fa6ab66f2b9cbfb33aadc98
Rafael Idrisov <rafael.idrisov@gmail.com>
2023-07-19 00:02:36 +0300

    Home PC 19.07.2023 00:02

--- process-mgm/chunks/top-of-stack.c
+++ process-mgm/chunks/top-of-stack.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// extern char etext, edata, end;
+
 int
 main(void) {
     void *stack_addr = __builtin_frame_address(0);
-    printf("Stack top's addres: %p\n", stack_addr);
+    printf("Stack top's addres:       %10p\n", stack_addr);
+
+    // printf("Program text (etext)      %10p\n", &etext);
+    // printf("Initialized data (edata)  %10p\n", &edata);
+    // printf("Unitialized data (end)    %10p\n", &end);
+
 
     return 0;
 }
