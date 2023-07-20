19c05be81481f507d9768c07b6c433974d1dac7e
Rafael Idrisov <rafael.idirisov@gmail.com>
2023-07-20 09:32:03 +0300

    Home PC 20.07.2023 09:32

--- process-mgm/chunks/execve.c
+++ process-mgm/chunks/execve.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
-void main() {
-   int pid;
-   char* args[] = {"execl.exe"}; // в массиве всего один элемент - один аргумент
-   pid = fork();
-   if (pid < 0) {
-    perror("fork() failed");
-   } else if (pid == 0) { /*Child*/
-       execve("/bin/ls", args, NULL);
-       perror("/bin/ls execve() failed"); 
-   } else { /*Parent*/
-       wait(NULL);
-       printf("Child exited with code: %d\n", WEXITSTATUS(pid));
-   }
+int
+main(void) {
+	char *binaryPath = "/bin/bash";
+	char *const args[] = {binaryPath, "-c", "echo \"Visit $HOSTNAME:$PORT from your browser.\"", NULL};
+	char *const env[] = {"HOSTNAME=www.linuxhint.com", "PORT=8080", NULL};
+
+	execve(binaryPath, args, env);
+
 }
